7cbb21e56f8d45911a7278b2f0ff0d7d9e2134e8
francisco-verdugo <franchuky@gmail.com>
2018-06-05 11:29:21 +0200

    Added quiz end check

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import QuizQuestion from "./QuizQuestion.js"
+import QuizEnd from './QuizEnd.js'
+
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
@@ -8,8 +10,11 @@ class Quiz extends Component {
     this.state = {quiz_position: 0};
  }   
  render() {
+     this.isQuizEnd = this.state.quiz_position === quizData.quiz_questions.length;
      return (
-        <div><QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position]}/></div>
+        <div>
+            <QuizEnd />
+            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position]}/></div>
      ); 
      
  }
