2ad4dbb692d8fb075ebe24f1c26c3f9e775cd40b
francisco-verdugo <franchuky@gmail.com>
2018-06-05 12:18:58 +0200

    Added reset quiz method

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react'
 
 class QuizEnd extends Component {
- render() {
-     return (
-        <div>
-        <p>Thanks for playing!</p>
-        <a href=''>Reset Quiz</a>
-      </div>   
-     );
- }
+
+    handleResetClick() {
+        this.props.resetClickHandler();
+
+    }
+
+    render() {
+        return (
+            <div>
+            <p>Thanks for playing!</p>
+            <a onClick={this.handleResetClick.bind(this)} href=''>Reset Quiz</a>
+        </div>   
+        );
+    }
 
 }
 export default QuizEnd
