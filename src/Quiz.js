7d13b6811f73f2ee539024cc6242cb04a0fffbd5
francisco-verdugo <franchuky@gmail.com>
2018-06-05 09:31:16 +0200

    Added question

--- src/Quiz.js
+++ src/Quiz.js
@@ -7,7 +7,10 @@ class Quiz extends Component {
     this.state = {quiz_position: 1};
  }   
  render () {
-     return <div>Quiz</div>
+     return 
+     <div>
+         <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+     </div>
  }
 }
 
